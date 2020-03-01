bcab22bace0ac0a09ba4d58ad3f5473b339c6ba6
root <root@images.os.selectel.org>
2020-03-01 17:42:44 +0000

    snap2htmlDailyUpdate

--- snap.sh
+++ snap.sh
@@ -1,10 +1,10 @@
-!/bin/bash
+﻿!/bin/bash
 cd /root/snap2html/DiogenesList-master/
 python /root/snap2html/DiogenesList-master/diogeneslist.py /root/01 index
 #sed -i "s#\[LINK ROOT\]#https://p.newo.cf#g"  /root/snap2html/DiogenesList-master/index.html
 #sed -i "s#\[LINK PROTOCOL\]##g"  /root/snap2html/DiogenesList-master/index.html
 #sed -i "s#\[SOURCE ROOT\]##g"  /root/snap2html/DiogenesList-master/index.html
-#sed -i "s#\\\\\\\root\\\\\\\01##g"  /root/snap2html/DiogenesList-master/index.html
+#sed -i "s#\\\\\\\root\\\\\\\rclone\\\\\\\01##g"  /root/snap2html/DiogenesList-master/index.html
 #sed -i "s#1>index#1>劳资转身就是一脚!#g" /root/snap2html/DiogenesList-master/index.html
 #sed -i "s#e>index#e>啊哒!#g" /root/snap2html/DiogenesList-master/index.html
 cp /root/snap2html/DiogenesList-master/index.html /root/snapGit/
