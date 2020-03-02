a15260527b8bdfa0e9446b562d7b37d0ea8683e3
ticn <mark@suuv.org>
2020-03-03 05:09:25 +0800

    snap2htmlDailyUpdate

--- snap.sh
+++ snap.sh
@@ -1,27 +1,14 @@
-﻿!/bin/bash
-
+#!/bin/bash
 cd /root/snap2html/DiogenesList-master/
-
-python /root/snap2html/DiogenesList-master/diogeneslist.py /root/mark index
-
+python /root/snap2html/DiogenesList-master/diogeneslist.py /root/cloudflare index
 #sed -i "s#\[LINK ROOT\]#https://su.marktd.workers.dev/#g"  /root/snap2html/DiogenesList-master/index.html
-
 #sed -i "s#\[LINK PROTOCOL\]##g"  /root/snap2html/DiogenesList-master/index.html
-
 #sed -i "s#\[SOURCE ROOT\]##g"  /root/snap2html/DiogenesList-master/index.html
-
 #sed -i "s#\\\\\\\root\\\\\\\rclone\\\\\\\00##g"  /root/snap2html/DiogenesList-master/index.html
-
 #sed -i "s#1>index#1>劳资转身就是一脚!#g" /root/snap2html/DiogenesList-master/index.html
-
 #sed -i "s#e>index#e>啊哒!#g" /root/snap2html/DiogenesList-master/index.html
-
 cp /root/snap2html/DiogenesList-master/index.html /root/snapGit/
-
 cd /root/snapGit
-
 git add .
-
 git commit -m 'snap2htmlDailyUpdate'
-
-git push  https://github.com/ticn/snap2html.git
+git push https://github.com/ticn/snap2html.git
