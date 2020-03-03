8f3ba3729c5a103a21bd299d99381755d63d6204
ticn <mark@suuv.org>
2020-03-03 15:30:34 +0800

    snap2htmlDailyUpdate

--- snap.sh
+++ snap.sh
@@ -5,8 +5,8 @@ python /root/snap2html/DiogenesList-master/diogeneslist.py /root/cloudflare inde
 #sed -i "s#\[LINK PROTOCOL\]##g"  /root/snap2html/DiogenesList-master/index.html
 #sed -i "s#\[SOURCE ROOT\]##g"  /root/snap2html/DiogenesList-master/index.html
 #sed -i "s#\\\\\\\root\\\\\\\rclone\\\\\\\00##g"  /root/snap2html/DiogenesList-master/index.html
-#sed -i "s#1>index#1>劳资转身就是一脚!#g" /root/snap2html/DiogenesList-master/index.html
-#sed -i "s#e>index#e>啊哒!#g" /root/snap2html/DiogenesList-master/index.html
+#sed -i "s#1>index#1>下次还敢.#g" /root/snap2html/DiogenesList-master/index.html
+#sed -i "s#e>index#e>知错不改，#g" /root/snap2html/DiogenesList-master/index.html
 cp /root/snap2html/DiogenesList-master/index.html /root/snapGit/
 cd /root/snapGit
 git add .
